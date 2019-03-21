3f7b9ae1455c2231811b8ac75015955b09f3c0d0
Caleb Bauermeister <cbauerman@gmail.com>
2019-03-20 17:35:06 -0700

    Initial Move

--- client/my-sites/email/email-management/placeholder.jsx
+++ client/my-sites/email/email-management/placeholder.jsx
@@ -14,10 +14,10 @@ import SectionHeader from 'components/section-header';
 import GoogleAppsUserItem from './google-apps-user-item';
 
 const Placeholder = () => (
-	<div className="email__google-apps-users-card is-placeholder">
+	<div className="email-management__gsuite-users-card is-placeholder">
 		<SectionHeader label={ 'G Suite Users' } />
-		<CompactCard className="email__google-apps-users-card-user-list">
-			<ul className="email__google-apps-users-card-user-list-inner">
+		<CompactCard className="email-management__gsuite-users-card-user-list">
+			<ul className="email-management__gsuite-users-card-user-list-inner">
 				<GoogleAppsUserItem user={ { email: 'mail@example.com', domain: 'example.com' } } />
 			</ul>
 		</CompactCard>
